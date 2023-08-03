abc339edccc06bd10aaf2d2aff7f1e97f6f938df
cjgaliana <cjgaliana@gmail.com>
2023-08-03 15:03:19 +0100

    feat: add basic server security

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+
+  // Set the root path to "api"
+  // Updated: Since we are hosting the api in a different domain,
+  // this root prefix is no longer needed
+  // app.setGlobalPrefix("api");
+
+  // Configure EXPRESS server
+  app.enableCors();
+  app.use(helmet());
+  app.use(compression());
+
+  await app.listen(process.env.PORT || 5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
