02783ce43d771fead7cd426572eea5c5981ee84f
cjgaliana <cjgaliana@gmail.com>
2023-08-04 14:47:50 +0100

    feat: return real DB accounts

--- apps/api/src/accounts/accounts.module.ts
+++ apps/api/src/accounts/accounts.module.ts
@@ -4,10 +4,11 @@ import { AccountsResolver } from './accounts.resolver';
 import { InstitutionsModule } from 'src/institutions/institutions.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './models/account.model';
+import { AccountsRepository } from './accounts.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account]), InstitutionsModule],
-  providers: [AccountsService, AccountsResolver],
+  providers: [AccountsService, AccountsResolver, AccountsRepository],
   exports: [AccountsService],
 })
 export class AccountsModule {}
--- apps/api/src/accounts/accounts.service.ts
+++ apps/api/src/accounts/accounts.service.ts
@@ -1,47 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Account } from './models/account.model';
 import { AccountType } from './models/accountType.model';
-import { Institution } from 'src/institutions/models/institution.model';
+import { AccountsRepository } from './accounts.repository';
 
 @Injectable()
 export class AccountsService {
-  /**
-   * MOCK
-   * Put some real business logic here
-   * Left for demonstration purposes
-   */
+  constructor(private repository: AccountsRepository) {}
 
   async create(): Promise<Account> {
     return {} as any;
   }
 
   async findOneById(id: string): Promise<Account> {
-    return {} as any;
+    return this.repository.findById(id);
   }
 
   async findAll(): Promise<Account[]> {
-    return [
-      {
-        id: '123',
-        name: 'Account 1',
-        balance: 123.12,
-        type: AccountType.CURRENT_ACCOUNT,
-        institution: { id: 'd76559c6-e53c-48ed-83f1-832bf04a6eec' },
-        created_at: new Date(),
-        updated_at: new Date(),
-        deleted_at: new Date(),
-      },
-      {
-        id: '234',
-        name: 'Account 2',
-        balance: 22.12,
-        type: AccountType.SAVINGS,
-        institution: null,
-        created_at: new Date(),
-        updated_at: new Date(),
-        deleted_at: new Date(),
-      },
-    ] as Account[];
+    return this.repository.findAll();
   }
 
   async remove(id: string): Promise<boolean> {
