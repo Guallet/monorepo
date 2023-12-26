e7181c653e1eec0d689d7e2389aad942d4adf891
cjgaliana <cjgaliana@gmail.com>
2023-12-26 17:57:38 +0000

    chore: move the google secrets to .env

--- apps/api/src/core/auth/supertokens.config.ts
+++ apps/api/src/core/auth/supertokens.config.ts
@@ -40,32 +40,30 @@ export const recipeList = [
           thirdPartyId: 'google',
           clients: [
             {
-              // TODO: Replace these values with the ones in the .env file
-              clientId:
-                '1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com',
-              clientSecret: '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',
+              clientId: process.env.GOOGLE_CLIENT_ID,
+              clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             },
           ],
         },
       },
-      {
-        config: {
-          thirdPartyId: 'apple',
-          clients: [
-            {
-              clientId: '4398792-io.supertokens.example.service',
-              additionalConfig: {
-                keyId: '7M48Y4RYDL',
-                privateKey:
-                  '-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----',
-                teamId: 'YWQCXGJRJL',
-              },
-            },
-          ],
-        },
-      },
+      // {
+      //   config: {
+      //     thirdPartyId: 'apple',
+      //     clients: [
+      //       {
+      //         clientId: '4398792-io.supertokens.example.service',
+      //         additionalConfig: {
+      //           keyId: '7M48Y4RYDL',
+      //           privateKey:
+      //             '-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----',
+      //           teamId: 'YWQCXGJRJL',
+      //         },
+      //       },
+      //     ],
+      //   },
+      // },
     ],
-    contactMethod: 'EMAIL_OR_PHONE',
+    contactMethod: 'EMAIL',
     flowType: 'USER_INPUT_CODE_AND_MAGIC_LINK',
   }),
 ];
