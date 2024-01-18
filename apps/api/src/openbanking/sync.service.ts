0ef275cfc39bdc7462959bc0e6a99d9bf5749ceb
cjgaliana <cjgaliana@gmail.com>
2024-01-18 17:18:01 +0000

    refactor: CRON constants

--- apps/api/src/openbanking/sync.service.ts
+++ apps/api/src/openbanking/sync.service.ts
@@ -5,6 +5,10 @@ import { Repository } from 'typeorm';
 import { NordigenAccount } from './entities/nordigen-account.entity';
 import { OpenbankingService } from './openbanking.service';
 
+const CRON_JOB_SYNC_ACCOUNTS_NAME = 'cron.sync.accounts';
+const CRON_JOB_SYNC_INSTIUTIONS_NAME = 'cron.sync.institutions';
+const CRON_JOB_TIMEZONE = 'Europe/London';
+
 @Injectable()
 export class SyncService {
   private readonly logger = new Logger(SyncService.name);
@@ -16,8 +20,8 @@ export class SyncService {
   ) {}
 
   @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, {
-    name: 'cron.institutions.sync',
-    timeZone: 'Europe/London',
+    name: CRON_JOB_SYNC_INSTIUTIONS_NAME,
+    timeZone: CRON_JOB_TIMEZONE,
   })
   async syncOpenBankingInstitutions() {
     // for (const country of this.supportedCountries) {
@@ -39,8 +43,8 @@ export class SyncService {
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
-    name: 'cron.accounts.sync',
-    timeZone: 'Europe/London',
+    name: CRON_JOB_SYNC_ACCOUNTS_NAME,
+    timeZone: CRON_JOB_TIMEZONE,
   })
   syncAccountsCronJob() {
     this.logger.debug('Syncing accounts via cron job');
