5556ebf59e1c8c35463eb49c04afb2f282f00ea3
cjgaliana <cjgaliana@gmail.com>
2024-02-20 21:54:28 +0000

    feat: use env for supabase keys

--- apps/webapp/src/core/auth/supabase.ts
+++ apps/webapp/src/core/auth/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tmqyfmfilopdrhovpnrr.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtcXlmbWZpbG9wZHJob3ZwbnJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTExNTQ0NTYsImV4cCI6MjAwNjczMDQ1Nn0.clzAs88Sw_RfW4E24hfZUJKNaff15TXeVpYF8AfwO74";
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- apps/webapp/src/vite-env.d.ts
+++ apps/webapp/src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_KEY: string;
   readonly VITE_POSTHOG_ENABLED: string;
   readonly VITE_POSTHOG_API_URL: string;
   readonly VITE_POSTHOG_TOKEN: string;
