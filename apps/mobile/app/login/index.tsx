eca12c68f9db487d737159bd2982f6ec547d5f86
cjgaliana <cjgaliana@gmail.com>
2024-02-23 15:10:39 +0000

    feat: native google signin in RN

--- apps/mobile/app/login/index.tsx
+++ apps/mobile/app/login/index.tsx
@@ -2,6 +2,15 @@ import { supabase } from "@/auth/supabase";
 import { router } from "expo-router";
 import React, { useState } from "react";
 import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+import {
+  GoogleSignin,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+
+GoogleSignin.configure({
+  webClientId:
+    "218442007864-a1cu28dbsvqkjm3f6t1snh84f9hpj7ir.apps.googleusercontent.com",
+});
 
 export default function Auth() {
   const [email, setEmail] = useState("cjgaliana@gmail.com");
@@ -32,20 +41,36 @@ export default function Auth() {
     }
   }
 
-  async function signUpWithEmail() {
-    setLoading(true);
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-
-    if (error) Alert.alert(error.message);
-    if (!session)
-      Alert.alert("Please check your inbox for email verification!");
-    setLoading(false);
+  async function signUpWithGoogle() {
+    try {
+      setLoading(true);
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      if (userInfo.idToken) {
+        const { data, error } = await supabase.auth.signInWithIdToken({
+          provider: "google",
+          token: userInfo.idToken,
+        });
+        if (error) {
+          Alert.alert("Error Google Signin", JSON.stringify(error));
+        } else {
+          router.replace("/(app)/");
+        }
+      } else {
+        Alert.alert("Error Google Signin", "No idToken");
+      }
+    } catch (error: any) {
+      setLoading(false);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
   }
 
   return (
@@ -78,13 +103,13 @@ export default function Auth() {
           onPress={() => signInWithOtp()}
         />
       </View>
-      {/* <View style={styles.verticallySpaced}>
+      <View style={styles.verticallySpaced}>
         <Button
-          title="Sign up"
+          title="Sign in with google"
           disabled={loading}
-          onPress={() => signUpWithEmail()}
+          onPress={() => signUpWithGoogle()}
         />
-      </View> */}
+      </View>
     </View>
   );
 }
