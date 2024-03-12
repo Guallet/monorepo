68783ef470296ae50d72f040d2ae3d8c28875aa5
Camilo Galiana <2270748+cjgaliana@users.noreply.github.com>
2024-03-12 11:59:38 +0000

    chore: added hardcoded iso4217 currencies list to money lib

--- packages/guallet-money/src/models/iso_4217.ts
+++ packages/guallet-money/src/models/iso_4217.ts
@@ -1,34 +1,31 @@
-import { json } from "@iso4217/json";
+import { json } from "./../data/data";
 
 type IsoCurrency = {
   country: string;
   name: string;
   code: string;
-  decimalPlaces: string;
+  decimalPlaces: string | number;
 };
 
 // Thanks to https://github.com/Virtual-Finland-Development/codesets/blob/3de13cc1bec4641d6cf2f025c564c46f54ae4230/src/resources/library/ISO4217Currencies.ts
 // for this code
-const transformed = json["$data"][0]["$data"].reduce(
-  (acc: Record<string, IsoCurrency>, blob: any) => {
-    const blobData = blob["$data"];
-    const name =
-      blobData.find((item: any) => item["$name"] === "CcyNm")?.["$data"] ?? "";
-    const code =
-      blobData.find((item: any) => item["$name"] === "Ccy")?.["$data"] ?? "";
+const transformed = json.$data[0].$data.reduce(
+  (acc: Record<string, IsoCurrency>, blob) => {
+    const blobData = blob.$data;
+    const name = blobData.find((item) => item.$name === "CcyNm")?.$data ?? "";
+    const code = blobData.find((item) => item.$name === "Ccy")?.$data ?? "";
     const country =
-      blobData.find((item: any) => item["$name"] === "CtryNm")?.["$data"] ?? "";
+      blobData.find((item) => item.$name === "CtryNm")?.$data ?? "";
     const decimalPlaces =
-      blobData.find((item: any) => item["$name"] === "CcyMnrUnts")?.["$data"] ??
-      "";
+      blobData.find((item) => item.$name === "CcyMnrUnts")?.$data ?? "";
 
     if (typeof acc[code] === "undefined") {
       // Pick the first one in the dataset, prevent duplicate ids
       acc[code] = {
-        name,
-        code,
-        country,
-        decimalPlaces,
+        name: `${name}`,
+        code: `${code}`,
+        country: `${country}`,
+        decimalPlaces: decimalPlaces,
       };
     }
     return acc;
