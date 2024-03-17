c0a6f76dafbb789a0615d502debee971e55be6f2
cjgaliana <cjgaliana@gmail.com>
2024-03-17 21:28:02 +0000

    [API] Added basic budget controller

---
+++ apps/api/src/budgets/budgets.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Logger,
+} from '@nestjs/common';
+import { BudgetsService } from './budgets.service';
+import { CreateBudgetDto } from './dto/create-budget.dto';
+import { UpdateBudgetDto } from './dto/update-budget.dto';
+import { RequestUser } from 'src/core/auth/request-user.decorator';
+import { UserPrincipal } from 'src/core/auth/user-principal';
+
+@Controller('budgets')
+export class BudgetsController {
+  private readonly logger = new Logger(BudgetsController.name);
+
+  constructor(private readonly budgetsService: BudgetsService) {}
+
+  // @Post()
+  // create(@Body() createBudgetDto: CreateBudgetDto) {
+  //   return this.budgetsService.create(createBudgetDto);
+  // }
+
+  @Get()
+  findAll(@RequestUser() user: UserPrincipal) {
+    return this.budgetsService.findAllForUser(user.id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string, @RequestUser() user: UserPrincipal) {
+    return this.budgetsService.findOneForUser({
+      id: id,
+      userId: user.id,
+    });
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateBudgetDto: UpdateBudgetDto) {
+  //   return this.budgetsService.update(+id, updateBudgetDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.budgetsService.remove(+id);
+  // }
+}
