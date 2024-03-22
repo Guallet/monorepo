e02ab4538005209b7473dc105a10c4b11733a1f1
cjgaliana <cjgaliana@gmail.com>
2024-03-22 16:09:13 +0000

    [Mobile] Added Jotai for management state for FLOW states

--- apps/mobile/app/(app)/accounts/create/index.tsx
+++ apps/mobile/app/(app)/accounts/create/index.tsx
@@ -1,4 +1,21 @@
+import { AccountTypeDto } from "@guallet/api-client";
+import { Currency } from "@guallet/money";
 import { Redirect } from "expo-router";
+import { atom } from "jotai";
+
+export type CreateAccountFlowState = {
+  accountName: string;
+  accountType: AccountTypeDto;
+  currency: Currency;
+  balance: number;
+};
+
+export const createAccountAtom = atom<CreateAccountFlowState>({
+  accountName: "",
+  accountType: AccountTypeDto.CURRENT_ACCOUNT,
+  currency: Currency.fromISOCode("GBP"),
+  balance: 0,
+});
 
 export default function CreateAccountScree() {
   return <Redirect href="/(app)/accounts/create/name" />;
