bdfecc3a819512157e63dcd1e01d2e0b915e9571
cjgaliana <cjgaliana@gmail.com>
2024-05-04 09:38:55 +0100

    [WebApp] Removed PostHog references

--- apps/webapp/src/vite-env.d.ts
+++ apps/webapp/src/vite-env.d.ts
@@ -3,9 +3,6 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_KEY: string;
-  readonly VITE_POSTHOG_ENABLED: string;
-  readonly VITE_POSTHOG_API_URL: string;
-  readonly VITE_POSTHOG_TOKEN: string;
 }
 
 interface ImportMeta {
