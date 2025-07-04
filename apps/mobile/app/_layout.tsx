89cd6510d4a23fd4d46b14772e1ef4d0f6a801b0
Camilo Galiana <cjgaliana@gmail.com>
2025-07-04 10:21:28 +0100

    feat: init expo 53 commit

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,95 +1,29 @@
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-import {
-  QueryClient,
-  QueryClientProvider,
-  focusManager,
-} from "@tanstack/react-query";
-import { useOnlineManager } from "@/hooks/useOnlineManager";
-import { useAppState } from "@/hooks/useAppState";
-import { AppStateStatus, Platform } from "react-native";
-import { AuthProvider } from "@/auth/useAuth";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { GualletClientProvider } from "@guallet/api-react";
-import { gualletClient } from "@/api/gualletClient";
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(app)",
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome6.font,
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
   if (!loaded) {
+    // Async font loading only occurs in development.
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function onAppStateChange(status: AppStateStatus) {
-  // React Query already supports in web browser refetch on window focus by default
-  if (Platform.OS !== "web") {
-    focusManager.setFocused(status === "active");
-  }
-}
-
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { retry: 2 } },
-});
-
-function RootLayoutNav() {
-  useOnlineManager();
-  useAppState(onAppStateChange);
-
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <GualletClientProvider client={gualletClient}>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              <BottomSheetModalProvider>
-                <Stack>
-                  <Stack.Screen name="(app)" options={{ headerShown: false }} />
-                  <Stack.Screen
-                    name="modal"
-                    options={{ presentation: "modal" }}
-                  />
-                </Stack>
-              </BottomSheetModalProvider>
-            </GestureHandlerRootView>
-          </GualletClientProvider>
-        </AuthProvider>
-      </QueryClientProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
