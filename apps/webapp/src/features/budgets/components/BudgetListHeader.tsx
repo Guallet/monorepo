5a2c228bac0a454f128c18ba07401d42814ad7af
Camilo Galiana <cjgaliana@gmail.com>
2025-08-23 09:47:05 +0100

    refactor(webapp): optimize budget calculations using useMemo in BudgetListHeader

--- apps/webapp/src/features/budgets/components/BudgetListHeader.tsx
+++ apps/webapp/src/features/budgets/components/BudgetListHeader.tsx
@@ -1,17 +1,27 @@
 import { BudgetDto } from "@guallet/api-client";
-import { Group, Text, Paper, Progress, Stack } from "@mantine/core";
+import { Group, Text } from "@mantine/core";
+import { useMemo } from "react";
 
 interface BudgetListHeaderProps {
   budgets: BudgetDto[];
 }
 
 export function BudgetListHeader({ budgets }: Readonly<BudgetListHeaderProps>) {
-  const totalBudget = budgets.reduce((acc, budget) => acc + budget.amount, 0);
-  const totalSpent = budgets.reduce((acc, budget) => acc + budget.spent, 0);
-  const remaining = totalBudget - totalSpent;
+  const { totalBudget, totalSpent, remaining } = useMemo(() => {
+    const totalBudget = budgets.reduce(
+      (acc, budget) => acc + Number(budget.amount),
+      0
+    );
+    const totalSpent = budgets.reduce(
+      (acc, budget) => acc + Number(budget.spent),
+      0
+    );
+    const remaining = totalBudget - totalSpent;
+    return { totalBudget, totalSpent, remaining };
+  }, [budgets]);
 
   return (
-    <Stack>
+    <Group justify="space-between" mb="md" align="center">
       <Group align="apart" mb="xs">
         <Text fz="md" fw={500}>
           Budget total
@@ -29,6 +39,6 @@ export function BudgetListHeader({ budgets }: Readonly<BudgetListHeaderProps>) {
           Left
         </Text>
       </Group>
-    </Stack>
+    </Group>
   );
 }
