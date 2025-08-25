32860caa3e74611e39df8812a808ad63880ff9a1
Camilo Galiana <cjgaliana@gmail.com>
2025-08-25 22:09:39 +0100

    feat(webapp): use code splitting for TanStackRouter

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import dns from "dns";
 
 // To enable path alias
@@ -12,52 +12,22 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-
   server: {
     port: 3000,
     strictPort: true,
     open: true,
   },
 
+  plugins: [
+    tanstackRouter({
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
-      "@components": fileURLToPath(
-        new URL("./src/components", import.meta.url)
-      ),
-      "@router": fileURLToPath(new URL("./src/router", import.meta.url)),
-      "@theme": fileURLToPath(new URL("./src/theme", import.meta.url)),
-      "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
-
-      // Features
-      "@accounts": fileURLToPath(
-        new URL("./src/features/accounts", import.meta.url)
-      ),
-      "@auth": fileURLToPath(new URL("./src/features/auth", import.meta.url)),
-      "@categories": fileURLToPath(
-        new URL("./src/features/categories", import.meta.url)
-      ),
-      "@connections": fileURLToPath(
-        new URL("./src/features/connections", import.meta.url)
-      ),
-      "@dashboard": fileURLToPath(
-        new URL("./src/features/dashboard", import.meta.url)
-      ),
-      "@reports": fileURLToPath(
-        new URL("./src/features/reports", import.meta.url)
-      ),
-      "@settings": fileURLToPath(
-        new URL("./src/features/settings", import.meta.url)
-      ),
-      "@tools": fileURLToPath(new URL("./src/features/tools", import.meta.url)),
-      "@transactions": fileURLToPath(
-        new URL("./src/features/transactions", import.meta.url)
-      ),
-      "@user": fileURLToPath(new URL("./src/features/user", import.meta.url)),
-
-      // "@guallet/ui-react": "/src/ui-react",
     },
   },
 });
