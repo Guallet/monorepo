0ec5922b090711182994698dc6dba038e02a4b09
Camilo Galiana <cjgaliana@gmail.com>
2025-08-29 21:57:59 +0100

    feat(webapp): add logout functionality to UserSettingsCard component

--- apps/webapp/src/features/settings/components/UserSettingsCard.tsx
+++ apps/webapp/src/features/settings/components/UserSettingsCard.tsx
@@ -1,9 +1,13 @@
 import { AppSection } from "@/components/Cards/AppSection";
 import { useUser } from "@guallet/api-react";
 import { Avatar, Button, Group, Stack, Text } from "@mantine/core";
+import { useNavigate } from "@tanstack/react-router";
 
 export function UserSettingsCard() {
+  // TODO: Replace this with a prop callback rather than navigate directly from here
+  const navigate = useNavigate();
   const { user } = useUser();
+
   return (
     <AppSection title="User Information">
       <Stack>
@@ -27,6 +31,15 @@ export function UserSettingsCard() {
           }}
         >
           Edit
+        </Button>
+        <Button
+          variant="outline"
+          color="red"
+          onClick={() => {
+            navigate({ to: "/logout" });
+          }}
+        >
+          Log out
         </Button>
       </Stack>
     </AppSection>
