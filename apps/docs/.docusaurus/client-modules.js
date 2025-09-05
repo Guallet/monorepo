3c6f90622301c0cce5e7bb9bfb6cdc09135dd063
Camilo Galiana <cjgaliana@gmail.com>
2025-09-05 09:37:46 +0100

    chore(repo): remove docs app remaining files

--- apps/docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/cjgaliana/src/github/guallet/monorepo/apps/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/cjgaliana/src/github/guallet/monorepo/apps/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/cjgaliana/src/github/guallet/monorepo/apps/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/cjgaliana/src/github/guallet/monorepo/apps/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/cjgaliana/src/github/guallet/monorepo/apps/docs/src/css/custom.css"),
-];
