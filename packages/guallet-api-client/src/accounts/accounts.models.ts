97ce3b2349674ebb8dad7df8aac7de0b819ac824
Camilo Galiana <cjgaliana@gmail.com>
2026-03-01 12:58:46 +0000

    feat(api): add Mortgage and Loan properties to AccountDto

--- packages/guallet-api-client/src/accounts/accounts.models.ts
+++ packages/guallet-api-client/src/accounts/accounts.models.ts
@@ -11,6 +11,8 @@ export type AccountDto = {
     | CurrentAccountProperties
     | CreditCardProperties
     | SavingAccountProperties
+    | MortgageAccountProperties
+    | LoanAccountProperties
     | null;
 };
 
