f6297c99d94190fdec8f1d8813b8b4bb182e476d
Camilo Galiana <cjgaliana@gmail.com>
2026-03-02 21:48:33 +0000

    chore(mobile): update dependencies

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -5,6 +5,10 @@ import * as Sentry from '@sentry/react-native';
 import { GualletApp } from '@/components/GualletApp';
 import { initAnalytics } from '@/utils/analytics';
 
+console.log('Running app with config:', {
+  isDev: __DEV__,
+});
+
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   debug: __DEV__,
