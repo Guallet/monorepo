f397d8e09b1086116581484051e006c74f86646e
Camilo Galiana <cjgaliana@gmail.com>
2026-03-07 21:19:12 +0000

    feat(webapp): added new vite+shadcn initial project

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -14,7 +14,7 @@ dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
     strictPort: true,
     open: true,
   },
