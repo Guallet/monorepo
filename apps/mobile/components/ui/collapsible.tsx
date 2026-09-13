e4c67ecfdf44f4756a1235ab849d3218bd9a2d20
Camilo Galiana <cjgaliana@gmail.com>
2026-09-13 21:25:20 +0100

    chore(mobile): tweaks

--- apps/mobile/components/ui/collapsible.tsx
+++ apps/mobile/components/ui/collapsible.tsx
@@ -11,7 +11,7 @@ export function Collapsible({
   title,
 }: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
-  const theme = useColorScheme() === 'dark' ? 'dark' : 'light';
+  const theme = useColorScheme() ;
 
   return (
     <ThemedView>
