0cb7701982c02e131ea79c07252f3410d86938bf
Camilo Galiana <cjgaliana@gmail.com>
2026-09-20 22:06:30 +0100

    feat(mobile): update Sentry config

--- apps/mobile/libs/sentry.ts
+++ apps/mobile/libs/sentry.ts
@@ -12,4 +12,18 @@ Sentry.init({
     }),
   ],
   enableNativeFramesTracking: !isRunningInExpoGo(),
+  // Enable logs to be sent to Sentry
+  // Learn more at https://docs.sentry.io/platforms/react-native/logs/
+  enableLogs: true,
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+  // We recommend adjusting this value in production.
+  // Learn more at
+  // https://docs.sentry.io/platforms/react-native/configuration/options/#traces-sample-rate
+  tracesSampleRate: 1.0,
+  // profilesSampleRate is relative to tracesSampleRate.
+  // Here, we'll capture profiles for 100% of transactions.
+  profilesSampleRate: 1.0,
+  // Record session replays for 100% of errors and 10% of sessions
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
 });
